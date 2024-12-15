16943345ec2b9ef0e3e25174ebe50e9530fe063a
r4Y07734 <ottoemil.heiskanen@gmail.com>
2024-12-15 19:31:29 +0200

    full movement and deltatime for player

--- js/app.js
+++ js/app.js
@@ -6,27 +6,57 @@ canvas.width = 1536;
 
 class Player {
     constructor() {
-        this.x = 10;
-        this.y = 10;
+        this.x = 50;
+        this.y = 50;
         this.width = 50;
         this.height = 50;
+        this.radius = 25;
         this.color = "#000";
-        this.speed = 5;
+        this.speed = 25;
         this.dx = 0;
+        this.dy = 0;
     }
 
-    update() {
-        this.x += this.dx;
+    update(deltaTime) {
+        if (!deltaTime) return;
 
-        if (this.x < 0) this.x = 0;
-        if (this.x + this.width > 1536) this.x = 1536 - this.width;
-        if (this.y < 0) this.y = 0;
-        if (this.y + this.height > 731) this.y = 731 - this.height;
+        const normalizedSpeed = this.speed * (deltaTime / 1000);
+        this.x += this.dx * normalizedSpeed;
+        this.y += this.dy * normalizedSpeed;
+
+        if (this.x - this.width / 2 < 0) this.x = 0 + this.width / 2;
+        if (this.x + this.width / 2 > 1536) this.x = 1536 - this.width / 2;
+        if (this.y - this.height / 2< 0) this.y = 0 + this.height / 2;
+        if (this.y + this.height / 2 > 731) this.y = 731 - this.height / 2;
     }
 
     draw(ctx) {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+    }
+
+    updatePlayerVelocity(keys) {
+        if (keys.d && keys.a || keys.ArrowLeft && keys.ArrowRight || keys.d && keys.ArrowLeft || keys.a && keys.ArrowRight) {
+            player.dx = 0;
+        } else if (keys.a || keys.ArrowLeft) {
+            player.dx = -player.speed;
+        } else if (keys.d || keys.ArrowRight) {
+            player.dx = player.speed;
+        } else {
+            player.dx = 0;
+        }
+
+        if (keys.w && keys.s || keys.ArrowUp && keys.ArrowDown || keys.w && keys.ArrowDown || keys.s && keys.ArrowUp) {
+            player.dy = 0;
+        } else if (keys.w || keys.ArrowUp) {
+            player.dy = -player.speed;
+        } else if (keys.s || keys.ArrowDown) {
+            player.dy = player.speed;
+        } else {
+            player.dy = 0;
+        }
     }
 }
 
@@ -37,37 +67,34 @@ const keys = {
     a: false,
     w: false,
     s: false,
+    ArrowDown: false,
+    ArrowUp: false,
+    ArrowRight: false,
+    ArrowLeft: false,
 };
-
-function updatePlayerVelocity() {
-    if (keys.d && keys.a) {
-        player.dx = 0;
-    } else if (keys.a) {
-        player.dx = -player.speed;
-    } else if (keys.d) {
-        player.dx = player.speed;
-    } else {
-        player.dx = 0;
-    }
-}
 
 document.addEventListener("keydown", (e) => {
     if (e.key in keys) keys[e.key] = true;
-    updatePlayerVelocity();
+    player.updatePlayerVelocity(keys);
 });
 
 document.addEventListener("keyup", (e) => {
     if (e.key in keys) keys[e.key] = false;
-    updatePlayerVelocity();
+    player.updatePlayerVelocity(keys);
 })
 
-function gameLoop() {
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+    const deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    player.update();
+    player.update(deltaTime);
     player.draw(ctx);
 
     requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+gameLoop(0);
